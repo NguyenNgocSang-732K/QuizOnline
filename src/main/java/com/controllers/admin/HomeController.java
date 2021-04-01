fd0960e2c1707178fd5967c5792019f265fc272d
Ngoc Tu <giabang205@gmail.com>
2021-04-01 23:17:56 +0700

    update database + generate entity

--- src/main/java/com/controllers/admin/HomeController.java
+++ src/main/java/com/controllers/admin/HomeController.java
@@ -1,16 +1,11 @@
 package com.controllers.admin;
 
-import java.util.Date;
-import java.util.HashSet;
-
-import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.model.entities.Account;
-import com.model.entities.Classes;
 import com.model.models.LoginModel;
 
 @Controller
@@ -22,7 +17,7 @@ public class HomeController extends AdminBaseController {
 		
 //		String pw_hash = BCrypt.hashpw("123", BCrypt.gensalt());
 //		System.out.println(pw_hash);
-		
+		UserDetails a =  iAccountService.loadUserByUsername("ngoctu");
 		modelMap.put("loginModel", loginModel);
 		return "account/admin/login";
 	}
