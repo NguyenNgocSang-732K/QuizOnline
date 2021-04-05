f7def55cb2f52429bb09f46dbd6825196bb48dde
Ngoc Tu <giabang205@gmail.com>
2021-04-06 01:54:05 +0700

    fixed merge with main

--- src/main/java/com/controllers/admin/ManageQuestionController.java
+++ src/main/java/com/controllers/admin/ManageQuestionController.java
@@ -1,9 +1,13 @@
 package com.controllers.admin;
 
+import com.constant.GeneralTypeEnum;
+import com.model.entityModels.PaginationModel;
+import com.model.entityModels.QuestionModel;
 import com.services.IQuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,11 +19,14 @@ public class ManageQuestionController extends AdminBaseController {
     IQuestionService _questionService;
 
     @RequestMapping(value = {"questions"}, method = RequestMethod.GET)
-    public String Index(ModelMap modelmap, @RequestParam(required = false, defaultValue = "0") String page,
+    public String Index(ModelMap modelmap,
+                        @RequestParam(required = false, defaultValue = "1") String page,
                         @RequestParam(required = false, defaultValue = "") String searchText) {
 
-
-        modelmap.put("questionModels", _questionService.GetAll(Integer.parseInt(page), 10, searchText));
-        return "admin/ManageQuestion";
+        modelmap.put("questionModels", _questionService.GetAll(Integer.parseInt(page),
+                GeneralTypeEnum.PAGESIZE,
+                searchText));
+        modelmap.put("searchText", searchText);
+        return "admin/IndexQuestion";
     }
 }
